2c67c98c79682931e615b344250b7b5eaafa6d1b
Lino Veloso <linoc.veloso@gmail.com>
2019-10-14 21:21:53 -0300

    Ambiente e conceitos, finalizando a parte de CRUD

--- aulas/modulo01/index.js
+++ aulas/modulo01/index.js
@@ -1,23 +1,72 @@
 const express = require('express');
+
 const server = express();
-// Query params = ?teste=1
-// Route params = /users/1
-// Request body = {"name": "Lino", "email", "lino.veloso@grad.ufsc.br"}
-//local
 
-server.get('/users', (req, res) => {
-  const nome = req.query.nome;
-  const email = req.query.email;
-  return res.json({
-    Nome: `Hello ${nome}`,
-    email: `seu email é: ${email}`
-  });
-})
+server.use(express.json());
 
-server.get('/users/:id', (req, res) => {
-  const id = req.params.id;
+const users = ['Lino', 'Cláudio', 'Karla'];
+// query Params = ?teste=1
+// Request body = {"name": Diego, "email": "lino.veloso@grad.ufsc.br"}
+
+//CRUD - Create, Read, Update, Delete
+
+server.get('/users', (req, res)=>{
+  return res.json(users)
+});
+/*
+server.get('/users', (req, res) =>{
+  const name = req.query.name;
+
   return res.json({
-    message: `Buscando dados do usuário ${id}`
+    message: `Hello ${name}`,
+    name: `Olá ${name}`,
+    endereco: {
+      rua: 'Jordelino João da Rosa',
+      obs: 'apartamento'
+    }
   })
+});
+*/
+// Route Params = /users/1
+
+server.get('/users/:index', (req, res) =>{
+  const { index } = req.params;
+
+  return res.json({
+    'name': users[index]
+  });
+});
+
+//Create
+
+server.post('/users', (req,res) =>{
+const { name } = req.body;
+
+users.push(name);
+return res.json(users)
+
 })
+
+//Update
+
+server.put('/users/:index', (req,res) => {
+const { index } = req.params;
+const { name } = req.body;
+
+users[index] = name;
+
+return res.json(users)
+
+})
+
+//Delete
+
+server.delete('/users/:index' , (req, res) =>{
+  const { index } = req.params;
+  users.splice(index, 1);
+
+  return res.json(users)
+
+})
+
 server.listen(3333);
