08ee2b3510691bcba1cb8f0bf8d7c0849da16837
velosobr <linoc.veloso@gmail.com>
2020-08-23 00:21:23 -0300

    added ts-node-dev

--- Eventos/Masterclass Typescript/backend/src/index.ts
+++ Eventos/Masterclass Typescript/backend/src/index.ts
@@ -1,10 +1,9 @@
-import * as express from 'express';
+import express from 'express';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-    return res.send('Hello World');
+    return res.send('Hello World 2');
 })
 
 app.listen(3333);
