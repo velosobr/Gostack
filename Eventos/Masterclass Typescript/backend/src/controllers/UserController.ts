8ca59ad06278126b324ccf6003d979716733d612
velosobr <linoc.veloso@gmail.com>
2020-08-23 23:22:22 -0300

    making a service using interfaces

--- Eventos/Masterclass Typescript/backend/src/controllers/UserController.ts
+++ Eventos/Masterclass Typescript/backend/src/controllers/UserController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import EmailService from "../services/EmailService";
 
 const users = [
-    {name: "Lino", email: "linoc.veloso@gmail.com"}
+    { name: "Lino", email: "linoc.veloso@gmail.com" }
 ];
 
 export default {
-    async index(req: Request, res: Response){
+    async index(req: Request, res: Response) {
         return res.json(users)
+    },
+
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService();
+
+        emailService.sendMail({ name: 'lino Veloso', email: 'Lino.veloso@gmail.com' },
+            { subject: 'Início das aulas', body: 'As aulas vão começar dia 31/08' }
+        );
+        return res.json("Email Enviado sucesso");
     }
+
+
 };
