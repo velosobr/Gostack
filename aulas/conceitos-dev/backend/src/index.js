a8af106bdb2fe1fe0caebd59b41e6318d851c5bb
velosobr <linoc.veloso@gmail.com>
2020-11-12 23:55:01 -0300

    Metodos HTTP

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const app = express();
-
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: CRiar uma informação no back
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+ 
 app.get('/projects', (req, res) => {
    return res.json({
       message: "Hello team"
    })
 });
 
-app.listen(3333);
+app.listen(3333, () =>{
+   console.log("🚀 back-end server started!.");
+});
