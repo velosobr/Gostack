b8f2afa9fd87a9546ca5b973093b4ecc29d3ea4c
velosobr <linoc.veloso@gmail.com>
2020-11-16 23:26:56 -0300

    improving back-end for mobileapp

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -12,42 +12,36 @@ app.use(express.json());
  * DELETE: Deletar uma informação no back-end
  */
 
- /**
-  * Tipos de parâmetros:
-  *  
-  * Query Params: Filtros e paginação
-  * Route Params: Identificar recursos (atualizar/deletar)
-  * Request Body: Conteudo na hora de criar ou editar um recurso (JSON)
-  */
- 
+/**
+ * Tipos de parâmetros:
+ *  
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (atualizar/deletar)
+ * Request Body: Conteudo na hora de criar ou editar um recurso (JSON)
+ */
 
-  const projects = [];
-  
+
+const projects = [];
+
 app.get('/projects', (req, res) => {
-//const {title, owner} = req.query;
+   //const {title, owner} = req.query;
 
-//console.log(title);
-//console.log(owner);
-  
-return res.json(projects);
+   //console.log(title);
+   //console.log(owner);
+
+   return res.json(projects);
 });
 
 app.post('/projects', (req, res) => {
-const {title, owner} = req.body
-const body = req.body;
+   const { title, owner } = req.body
+   const body = req.body;
+   projects.push({ body })
+   return res.json(projects);
 
-console.log(title);
-console.log(owner);
-console.log(body);
-   return res.json([
-      'Project 1',
-      'project 2',
-      'Project 3'
-   ]);
 })
 
 app.put('/projects/:id', (req, res) => {
-   const {id} = req.params;
+   const { id } = req.params;
 
    console.log(id);
 
@@ -64,6 +58,6 @@ app.delete('/projects/:id', (req, res) => {
       'project 2',
    ])
 })
-app.listen(4444, () =>{
-   console.log("🚀 back-end server started!.");
+app.listen(4444, () => {
+   console.log("🚀 back-end server started on port 4444!.");
 });
